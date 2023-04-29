bb2e42d83085b39df639dbfdf85df77f77779041
zhangcm0719 <zhangcm0719@163.com>
2023-04-29 22:24:02 +0800

    feat: add monitor-data

---
+++ monitor-data/src/main/java/org/zhcmi/monitor/data/MonitorDataApplication.java
@@ -0,0 +1,13 @@
+package org.zhcmi.monitor.data;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonitorDataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MonitorDataApplication.class, args);
+    }
+
+}
